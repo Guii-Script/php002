193394b65f8ca6da98c8359b02fdb373d782433c
Guilherme Oliveira <guilhermedepaula1623@gmail.com>
2025-05-14 22:14:18 -0300

    Atividades

---
+++ lista_atividades/exercicio005/exercicio5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 5</title>
+</head>
+<body>
+    <form name="formulario" method="post" action="verificar.php">
+        Digite um número: <input type="number" name="numero">
+        <button type="submit">Verificar Tabuada</button>
+    </form> 
+</body>
+</html>
